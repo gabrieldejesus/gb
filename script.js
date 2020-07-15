9462521086edcc15df9420860aa583fd9c4ec8f8
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-15 19:00:16 -0300

    new features added

--- script.js
+++ script.js
@@ -1,17 +1,44 @@
 let formulario = document.getElementById('formulario');
 let campo = document.getElementById('campo');
-let name= '';
+let name= "";
+let message = "";
+
 
 formulario.addEventListener('submit', function(e) {
     name = campo.value; // pegando o valor do campo
-    document.querySelector('.gb-hi').classList.add('hide'); // remover pergunta
-    document.querySelector('.question').classList.add('hide'); // remover form
-    document.querySelector('.avatar-name').classList.add('show'); // mostrando a mensagem
-    document.querySelector('#name').innerHTML = name; // imprimir nome da pessoa
+
     
+
+    if(campo.value == 'tudo bem?') {
+        document.querySelector('#message-gb').innerHTML = 'Estou bem e você?';
+    } else {
+        document.querySelector('#message-gb').innerHTML = 'Não entendi';
+    }if (campo.value == 'estou bem') {
+        document.querySelector('#message-gb').innerHTML = 'Ai sim, Qual a boa meu cria?';
+    }
+
+    // ativando modo light e desativando
+    if(campo.value == 'ativar modo light') {
+        document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
+        document.querySelector('#message-gb').innerHTML = '🥴 Quase me ceguei carai';
+    } else {
+        if(campo.value == 'desativar modo light') {
+            document.querySelector('body').style.backgroundColor = 'var(--black-color)';
+            document.querySelector('#message-gb').innerHTML = '😎 Aulas!';
+        }
+    }
+
+    // ativando modo dark e desativando
+    if(campo.value == 'ativar modo dark') {
+        document.querySelector('body').style.backgroundColor = 'var(--black-color)';
+        document.querySelector('#message-gb').innerHTML = '😎 Esse é top!';
+    } else {
+        if(campo.value == 'desativar modo dark') {
+            document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
+            document.querySelector('#message-gb').innerHTML = '🥴 cAAAArai fiquei cego';
+        }
+    }
 
     //impede o envio do formulario
     e.preventDefault();
 }) 
-
-
