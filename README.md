b804be9d570d13d9d067191bebbfd6ac3bd27863
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-16 09:52:40 -0300

    fixing bugs in the script

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 > 📚 Dedicated project to test what I'm learning with javascript.
 
 ![](assets/web-preview.svg)
+
+### Some commands
+- Tudo bem?
+- Ativar modo light
+- Desativar modo light
+- Ativar modo dark
+- Desativar modo dark
--- script.js
+++ script.js
@@ -7,35 +7,39 @@ let message = "";
 formulario.addEventListener('submit', function(e) {
     name = campo.value; // pegando o valor do campo
 
-    
-
-    if(campo.value == 'tudo bem?') {
+    if(campo.value == 'Tudo bem?') {
         document.querySelector('#message-gb').innerHTML = 'Estou bem e você?';
+        campo.value = '';
     } else {
         document.querySelector('#message-gb').innerHTML = 'Não entendi';
-    }if (campo.value == 'estou bem') {
-        document.querySelector('#message-gb').innerHTML = 'Ai sim, Qual a boa meu cria?';
+    }if (campo.value == 'Estou bem') {
+        document.querySelector('#message-gb').innerHTML = 'Ai sim, Qual a boa meu mano?';
+        campo.value = '';
     }
 
     // ativando modo light e desativando
-    if(campo.value == 'ativar modo light') {
+    if(campo.value == 'Ativar modo light') {
         document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
-        document.querySelector('#message-gb').innerHTML = '🥴 Quase me ceguei carai';
+        document.querySelector('#message-gb').innerHTML = '🥴 Quase me ceguei kkkkk';
+        campo.value = '';
     } else {
         if(campo.value == 'desativar modo light') {
             document.querySelector('body').style.backgroundColor = 'var(--black-color)';
-            document.querySelector('#message-gb').innerHTML = '😎 Aulas!';
+            document.querySelector('#message-gb').innerHTML = '😎 Me amarro nesse!';
+            campo.value = '';
         }
     }
 
     // ativando modo dark e desativando
-    if(campo.value == 'ativar modo dark') {
+    if(campo.value == 'Ativar modo dark') {
         document.querySelector('body').style.backgroundColor = 'var(--black-color)';
         document.querySelector('#message-gb').innerHTML = '😎 Esse é top!';
+        campo.value = '';
     } else {
-        if(campo.value == 'desativar modo dark') {
+        if(campo.value == 'Desativar modo dark') {
             document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
-            document.querySelector('#message-gb').innerHTML = '🥴 cAAAArai fiquei cego';
+            document.querySelector('#message-gb').innerHTML = '🥴 Fiquei cegooo kkkk';
+            campo.value = '';
         }
     }
 
