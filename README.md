375667c47579be8d504751d81c316bfb23f0d9e2
Gabriel de Jesus <gabrieeldejesus@hotmail.com>
2020-07-16 19:37:56 -0300

    new features and functionality added

--- README.md
+++ README.md
@@ -3,8 +3,12 @@
 ![](assets/web-preview.svg)
 
 ### Some commands
+- Sim
+- Não
 - Tudo bem?
 - Ativar modo light
 - Desativar modo light
 - Ativar modo dark
 - Desativar modo dark
+- Que horas são?
+- O que preciso fazer hoje?
--- script.js
+++ script.js
@@ -1,47 +1,105 @@
-let formulario = document.getElementById('formulario');
-let campo = document.getElementById('campo');
+const c = (el)=>document.querySelector(el); // retornar o item
+const cs = (el)=>document.querySelectorAll(el); // retornar um array com os itens que ele achou
+
+let formulario = c('#formulario');
+let campo = c('#campo');
 let name= "";
 let message = "";
+let d = new Date(); // pegar data atual
+let today = d.getDate(); // pegar o dia atual
+let hours = d.getHours(); // pega a hora 
+let minutes = d.getMinutes(); // pegar os minutos
 
 
 formulario.addEventListener('submit', function(e) {
+
+
+// CONVERSATION
     name = campo.value; // pegando o valor do campo
 
-    if(campo.value == 'Tudo bem?') {
-        document.querySelector('#message-gb').innerHTML = 'Estou bem e você?';
+    if (campo.value == 'Tudo bem?') {
+        c('#message-gb').innerHTML = 'Estou bem e você?';
         campo.value = '';
     } else {
-        document.querySelector('#message-gb').innerHTML = 'Não entendi';
+        c('#message-gb').innerHTML = 'Calma ai que ainda não sei falar muito 😳<br> Tenta digitar outra coisa prf!!!🙏🏾 ';
     }if (campo.value == 'Estou bem') {
-        document.querySelector('#message-gb').innerHTML = 'Ai sim, Qual a boa meu mano?';
+        c('#message-gb').innerHTML = 'Ai sim, Fazendo o que de bom?';
         campo.value = '';
+    } else {
+        if(campo.value == 'Sim') {
+            c('#message-gb').innerHTML = 'Qual a boa?';
+            campo.value = '';
+        } else {
+            if(campo.value == 'Não') {
+                c('#message-gb').innerHTML = 'Nossa 😳 ok!';
+                campo.value = '';
+            }
+        }
+    }if(campo.value == 'Que horas são?') {
+        c('#message-gb').innerHTML = `São ${hours}:${minutes} `;
+        campo.value = '';
+    } else {
+        if(campo.value == 'Obrigado') {
+            c('#message-gb').innerHTML = 'Que nada Tmj! ❤️'
+            campo.value = '';
+        }
     }
 
+
+// RESOURCES
     // ativando modo light e desativando
     if(campo.value == 'Ativar modo light') {
-        document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
-        document.querySelector('#message-gb').innerHTML = '🥴 Quase me ceguei kkkkk';
+        c('body').style.backgroundColor = 'var(--mode-light)';
+        c('#message-gb').innerHTML = '🥴 Quase me ceguei kkkkk';
         campo.value = '';
     } else {
         if(campo.value == 'desativar modo light') {
-            document.querySelector('body').style.backgroundColor = 'var(--black-color)';
-            document.querySelector('#message-gb').innerHTML = '😎 Me amarro nesse!';
+            c('body').style.backgroundColor = 'var(--black-color)';
+            c('#message-gb').innerHTML = '😎 Me amarro nesse!';
             campo.value = '';
         }
     }
 
     // ativando modo dark e desativando
     if(campo.value == 'Ativar modo dark') {
-        document.querySelector('body').style.backgroundColor = 'var(--black-color)';
-        document.querySelector('#message-gb').innerHTML = '😎 Esse é top!';
+        c('body').style.backgroundColor = 'var(--black-color)';
+        c('#message-gb').innerHTML = '😎 Esse é top!';
         campo.value = '';
     } else {
         if(campo.value == 'Desativar modo dark') {
-            document.querySelector('body').style.backgroundColor = 'var(--mode-light)';
-            document.querySelector('#message-gb').innerHTML = '🥴 Fiquei cegooo kkkk';
+            c('body').style.backgroundColor = 'var(--mode-light)';
+            c('#message-gb').innerHTML = '🥴 Fiquei cegooo kkkk';
             campo.value = '';
         }
     }
+
+    // ativar tarefas
+    if(campo.value == 'O que preciso fazer hoje?') {
+        c('table').style.display = 'inline-table'; // mostrar tarefas
+
+        c('#message-gb').innerHTML = `Foco, Força e Fé meu mano kkkk <br> Hoje é o dia ${today} e aqui está sua lista de tarefas`; // mostrar mensagem + o dia de hoje
+        campo.value = '';
+    } else {
+        if(campo.value = 'O que preciso fazer hoje?') {
+            c('table').style.display = 'none'; // esconder tarefas
+            campo.value = '';
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     //impede o envio do formulario
     e.preventDefault();
